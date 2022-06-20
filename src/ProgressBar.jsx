028c0cfe853933d2692c9ac5b428d2363e234800
VietNguyenQuoc <nqviet157@gmail.com>
2022-06-20 18:37:29 +0700

    integrate postcss with tailwind and vite

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,16 +1,20 @@
 export function ProgressBar({ stepLabels = [], activeIdx = 0 }) {
   return (
     <div className="hidden md:block col-span-1 text-right fixed right-0 z-10">
-      {stepLabels.map((label, idx) => (
-        <div
-          key={label}
-          className={`pr-6 py-3 border-r-3 mr-8 border-r-white ${
-            idx !== activeIdx && "border-opacity-40"
-          } progress-item`}
-        >
-          {label}
-        </div>
-      ))}
+      {stepLabels.map((label, idx) => {
+        const isActive = idx === activeIdx;
+
+        return (
+          <div
+            key={label}
+            className={`pr-6 py-3 border-r-3 mr-8 ${
+              isActive ? "border-r-white" : "border-r-white/25"
+            } transition duration-200`}
+          >
+            {label}
+          </div>
+        );
+      })}
     </div>
   );
 }
